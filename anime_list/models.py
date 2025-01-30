8c9a0ea0423a7c4814c80bcf775c9388485a1ec3
Head-Shot-53 <denchikmashtack@gmail.com>
2025-01-30 21:57:13 +0200

    додав клас для категорії

--- anime_list/models.py
+++ anime_list/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Anime(models.Model):
     title = models.CharField(max_length=500) # назва
     description = models.TextField() # опис
     release_year = models.IntegerField() # дата виходу
     rating = models.FloatField() # рейтинг
     image_url = models.URLField(blank=True,null=True) # Посилання на обкладинку
+    categories = models.ManyToManyField(Category, related_name='animes')
 
     def __str__(self):
         return self.title # вивід назви у Djsngo admin
-    
     
