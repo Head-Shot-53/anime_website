222077b891d955f3ca4f43192911094650f3cdd6
Head-Shot-53 <denchikmashtack@gmail.com>
2025-01-30 22:58:02 +0200

    Save local changes

--- anime_site/urls.py
+++ anime_site/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('anime_list.urls')) # підключаю маршрути з anime_list
 ]
+
+# Це дозволяє відображати зображення в режимі розробки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
