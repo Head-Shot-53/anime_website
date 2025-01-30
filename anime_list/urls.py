a6e7a3a08f0a00cb6f064a0eabca36669de363a9
Head-Shot-53 <denchikmashtack@gmail.com>
2025-01-30 21:58:09 +0200

    додав маршрут для сторінки з категоріями

--- anime_list/urls.py
+++ anime_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import anime_list, anime_detail
+from .views import anime_list, anime_detail, anime_by_category
 
 urlpatterns = [
     path('', anime_list, name='anime_list'),
     path('<int:anime_id>/', anime_detail, name='anime_detail'),
+    path('category/<int:category_id>/',anime_by_category,name='anime_by_category')
 ]
