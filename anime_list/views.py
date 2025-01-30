360c767fd5cf6b3d00a0bcfa86feac427ad7b329
Head-Shot-53 <denchikmashtack@gmail.com>
2025-01-30 21:59:07 +0200

    додав функцію для роботи з анім за катеоріями

--- anime_list/views.py
+++ anime_list/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Anime
+from .models import Anime, Category
 
 def anime_list(request):
     animes = Anime.objects.all() # Отримування всіх аніме з бази
-    return render(request, 'anime_list/anime_list.html', {'animes':animes})
+    categories = Category.objects.all() # отримую всі категорії
+    return render(request, 'anime_list/anime_list.html', {'animes':animes, 'categories':categories})
  
 def anime_detail(request,anime_id):
     anime = get_object_or_404(Anime, id=anime_id)
     return render(request, 'anime_list/anime_detail.html', {'anime':anime})
 
+def anime_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    animes = category.animes.all()
+    return render(request, 'anime_list/anime_list.html', {'animes':animes, 'select_category':category})
