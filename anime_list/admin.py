f9ef8dbd0c0abb72385d88dd1fbda0d8bdc05864
Head-Shot-53 <denchikmashtack@gmail.com>
2025-02-01 21:14:51 +0200

    додав віджета в адмінку, щоб до аніме можна уло додавати декілька жанрів

--- anime_list/admin.py
+++ anime_list/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Anime, Category
 
-admin.site.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_year', 'rating')  # Поля, які будуть відображатися в списку
+    filter_horizontal = ('categories',)  # Додає зручний віджет для вибору кількох жанрів
+
+admin.site.register(Anime, AnimeAdmin)
 admin.site.register(Category)
