ffd907dd1d03656608ea27bf3ec0278b15a52873
Head-Shot-53 <denchikmashtack@gmail.com>
2025-02-04 15:50:36 +0200

    прибрав поислання aanime_by_category, бо вона стала не потрібна

--- anime_list/urls.py
+++ anime_list/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import anime_list, anime_detail, anime_by_category, category_detail
+from .views import anime_list, anime_detail, category_detail
 
 urlpatterns = [
     path('', anime_list, name='anime_list'),
     path('<int:anime_id>/', anime_detail, name='anime_detail'),
-    path('category/<int:category_id>/',anime_by_category,name='anime_by_category'),
+    # path('category/<int:category_id>/',anime_by_category,name='anime_by_category'),
     path('category/<int:category_id>/', category_detail, name='category_detail'),
 ]
