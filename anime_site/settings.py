031ee5b831dfa756570129122dd3008126408b81
Head-Shot-53 <denchikmashtack@gmail.com>
2025-02-05 20:24:54 +0200

    додав додаток users

--- anime_site/settings.py
+++ anime_site/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'anime_list'
+    'anime_list',
+    'users'
 ]
 
 MIDDLEWARE = [
