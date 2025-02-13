73696730e3ad886b1580250cdadf5c8d73dad414
Head-Shot-53 <denchikmashtack@gmail.com>
2025-02-13 21:03:31 +0200

    змінив посилання  на вихід

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register, profile
+from .views import register, profile, user_logout
 
 urlpatterns = [
     path('register/', register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(next_page='home'),name='logout'),
+    # path('logout/',auth_views.LogoutView.as_view(next_page='home'),name='logout'),
     path('profile/',profile,name='profile'),
+    path('logout/', user_logout, name='logout'),
 ]
